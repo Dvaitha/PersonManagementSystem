dafa6df1a0c64075f1d8eb3ba474ba75d2b7ffcc
Dvaitha <34769208+Dvaitha@users.noreply.github.com>
2020-02-24 11:01:42 +0530

    Updated Forgot Password

--- forgot-password.php
+++ forgot-password.php
@@ -67,7 +67,7 @@ if(isset($_POST['submit']))
                     <div class="login-content">
                         <div class="login-logo">
                             <a href="#" style="font-size:24px;">
-                                Company Visitor Management System (pms)
+                                Person Management System (pms)
                             </a>
                         </div>
                         <p style="font-size:16px; color:red" align="center"> <?php if($msg){
